9f6e06b0a8c19ee97343185b85ff6bad6ad55fda
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-09 13:08:55 +0200

    Commiting employee CRUD

---
+++ Logic/EmployeeService.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class EmployeeService
+    {
+        EmployeeDAO employeeDAO;
+
+        public EmployeeService()
+        {
+            employeeDAO = new EmployeeDAO();
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            employeeDAO.CreateEmployee(employee);
+        }
+        
+        public List<Employee> GetEmployees()
+        {
+            return employeeDAO.GetEmployees();
+        }
+
+        public Employee GetEmployee(string id)
+        {
+            return employeeDAO.GetEmployee(id);
+        }
+
+        public void DeleteEmployee(string id)
+        {
+            employeeDAO.DeleteEmployee(id);
+        }
+    }
+}
