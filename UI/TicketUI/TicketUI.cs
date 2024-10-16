d5eab74f613a570e9a77b47cae60f73a32a8e8ec
Leon Watertor <68647023+iolite312@users.noreply.github.com>
2024-10-16 10:08:58 +0200

    Update TicketUI.cs

--- UI/TicketUI/TicketUI.cs
+++ UI/TicketUI/TicketUI.cs
@@ -1,33 +1,25 @@
 ﻿using Logic;
 using Model;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace UI.TicketUI
 {
     public partial class TicketUI : Form
     {
-        TicketService ticketService;
+        
         public TicketUI()
         {
             InitializeComponent();
-            ticketService = new TicketService();
             InitListView();
         }
         private void InitListView()
         {
             ticketListView.Items.Clear();
+            TicketService ticketService = new TicketService();
             List<Ticket> tickets = ticketService.GetTickets();
 
-            tickets = tickets.OrderBy(ticket => ticket.Status)
-                        .ThenBy(ticket => ticket.Deadline)
+            tickets = tickets.OrderBy(ticket => ticket.Deadline)
+                        .ThenBy(ticket => ticket.Status)
                         .ToList();
 
             foreach (Ticket ticket in tickets)
