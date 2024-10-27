b99c24984a89da419d9a07c37b7a4519c26bd6f3
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-10-27 13:32:07 +0100

    More DTO objects for creating and displaying data

---
+++ Api/DTO/TicketDTO.cs
@@ -0,0 +1,52 @@
+﻿using Model.Enums;
+using Model;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Api.DTO
+{
+    public class TicketDTO
+    {
+        public string CaseName { get; set; }
+
+        public ETicketType Type { get; set; }
+
+        public ETicketStatus Status { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public ETicketPriority Priority { get; set; }
+
+        public Handler Handler { get; set; }
+
+        public TicketEmployeeDTO Employee { get; set; }
+
+        public int Deadline { get; set; }
+
+        public Ticket RevertModel()
+        {
+            return new Ticket(CaseName, Type, Status, Description, Timestamp, Priority, Handler, Employee.RevertModel(), Deadline);
+        }
+        public Ticket RevertModel(string id)
+        {
+            return new Ticket(CaseName, Type, Status, Description, Timestamp, Priority, Handler, Employee.RevertModel(), Deadline, id);
+        }
+
+        public TicketDTO ConvertModel(Ticket ticket)
+        {
+            CaseName = ticket.CaseName;
+            Type = ticket.Type;
+            Status = ticket.Status;
+            Description = ticket.Description;
+            Timestamp = ticket.Timestamp;
+            Priority = ticket.Priority;
+            Handler = ticket.Handler;
+            Employee = Employee.ConvertModel(ticket.Employee);
+            Deadline = ticket.Deadline;
+            return this;
+        }
+
+    }
+}
---
+++ Api/DTO/TicketEmployeeDTO.cs
@@ -0,0 +1,38 @@
+﻿using Model.Enums;
+using Model;
+using System.Text.Json.Serialization;
+
+namespace Api.DTO
+{
+    public class TicketEmployeeDTO
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public ERole Role { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public EDepartment Department { get; set; }
+
+        public TicketEmployeeDTO ConvertModel(Employee employee)
+        {
+            Id = employee.Id;
+            FirstName = employee.FirstName;
+            LastName = employee.LastName;   
+            Email = employee.Email;
+            Role = employee.Role;
+            Department = employee.Department;
+            return this;
+        }
+
+        public Employee RevertModel()
+        {
+            return new Employee(FirstName, LastName, Email, Role, Department, null, Id);
+        }
+    }
+}
